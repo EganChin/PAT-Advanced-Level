9f5808b84555bf2778704f0209cac261af559f13
egan chen <1154610080@qq.com>
2019-07-31 09:15:24 +0800

    完成string专项练习:1060 Are They equals

--- STL/1060 Are They Equal (25).cpp
+++ STL/1060 Are They Equal (25).cpp
@@ -22,7 +22,71 @@ Sample Input 1:
 Sample Output 1:
 YES 0.123*10^5
 Sample Input 2:
-3 120 128
+
 Sample Output 2:
 NO 0.120*10^3 0.128*10^3
 **/
+
+//2019.7.30 16:57 reading
+//2019.7.30 17:00 read
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+
+string deal(string s, int &exp) {
+    int digit = 0;
+    //去掉小数点前的前导零
+    while (s.length() > 0 && s[0] == '0')
+        s.erase(s.begin());
+    //去掉小数点后的前导零
+    if (s[0] == '.') {
+        s.erase(s.begin());
+        while (s.length() > 0 && s[0] == '0') {
+            s.erase(s.begin());
+            exp--;
+        }
+    } else {
+        //寻找小数点
+        while (digit < s.length() && s[digit] != '.') {
+            exp++;
+            digit++;
+        }
+        //如果遇到小数点，将其删除
+        if (digit < s.length())
+            s.erase(s.begin() + digit);
+    }
+    //如果去掉小数点和其前后的前导零后长度为零，说明等于0
+    if (s.length() == 0)
+        exp = 0;
+
+    string result;
+    result = "0." + result;
+    for (int i = 0, j = 0; i < n; i++) {
+        if (i < s.length()) result += s[j++];
+            //补零
+        else result += "0";
+    }
+    return result;
+}
+
+int main() {
+    string a, b;
+    cin >> n >> a >> b;
+
+    int exp1, exp2;
+    a = deal(a, exp1) +"*10^"+exp1;
+    b = deal(b, exp2);
+
+    if (a == b && exp1==exp2)
+        cout << "YES " << a + "*10^" << exp1 << endl;
+    else
+        cout << "NO " << a + "*10^" << exp1 << " " << b + "*10^" << exp2 << endl;
+//    printf("%s,%s\n", a.c_str(), b.c_str());
+}
+
+//7:31 9:09 NC AC
+//7:31 9:13 PAT AC(exp1&exp2 uninitialized)
