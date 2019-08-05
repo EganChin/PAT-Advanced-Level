2ce17286935d6af89a44e0295b0c0036bdb084d9
egan chen <1154610080@qq.com>
2019-08-05 17:08:06 +0800

    完成数据结构专项练习:1051 Pop Sequence

---
+++ Data-Structure/1051 Pop Sequence.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Administrator on 2019/8/5.
+//
+
+/**
+ * Given a stack which can keep M numbers at most. Push N numbers in the order of 1, 2, 3, ..., N and pop randomly. You are supposed to tell if a given sequence of numbers is a possible pop sequence of the stack. For example, if M is 5 and N is 7, we can obtain 1, 2, 3, 4, 5, 6, 7 from the stack, but not 3, 2, 1, 7, 5, 6, 4.
+
+Input Specification:
+Each input file contains one test case. For each case, the first line contains 3 numbers (all no more than 1000): M (the maximum capacity of the stack), N (the length of push sequence), and K (the number of pop sequences to be checked). Then K lines follow, each contains a pop sequence of N numbers. All the numbers in a line are separated by a space.
+
+Output Specification:
+For each pop sequence, print in one line "YES" if it is indeed a possible pop sequence of the stack, or "NO" if not.
+
+Sample Input:
+5 7 5
+1 2 3 4 5 6 7
+3 2 1 7 5 6 4
+7 6 5 4 3 2 1
+5 6 4 3 7 2 1
+1 7 6 5 4 3 2
+Sample Output:
+YES
+NO
+NO
+YES
+NO
+ */
+
+//2019.8.5 16:25 read
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int m, n, k;
+    scanf("%d%d%d", &m, &n, &k);
+
+    string ans[k];
+
+    for (int i = 0; i < k; i++) {
+        int pop[n + 10];
+        stack<int> s;
+        for (int j = 1; j <= n; j++) {
+            scanf("%d", &pop[j]);
+        }
+        for (int j = 1, t = 1; j <= n || t <= n;) {
+            if (!s.empty() && s.top() == pop[t]) {
+                //printf("pop %d, ", s.top());
+                s.pop();
+                t++;
+            } else {
+                s.push(j);
+                //printf("push %d,", j);
+                j++;
+                if (s.size() > m) {
+                    break;
+                }
+            }
+        }
+        ans[i] = s.empty() ? "YES" : "NO";
+        //printf("\n");
+    }
+
+    for (int i = 0; i < k; i++)
+        cout << ans[i] << endl;
+}
+
+//17:06 AC
