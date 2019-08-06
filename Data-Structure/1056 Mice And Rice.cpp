ff04b166fea2f1bcf6100e97c3dfa62f06bf56d7
egan chen <1154610080@qq.com>
2019-08-06 15:41:56 +0800

    完成数据结构专项练习:1056 Mice And Rice

--- Data-Structure/1056 Mice And Rice.cpp
+++ Data-Structure/1056 Mice And Rice.cpp
@@ -2,47 +2,65 @@
 // Created by Administrator on 2019/8/5.
 //
 
-/**
- * Mice and Rice is the name of a programming contest in which each programmer must write a piece of code to control the movements of a mouse in a given map. The goal of each mouse is to eat as much rice as possible in order to become a FatMouse.
+//2019.8.5 17:13 reading
+//2019.8.6 14:58 read
 
-First the playing order is randomly decided for N
-​P
-​​  programmers. Then every N
-​G
-​​  programmers are grouped in a match. The fattest mouse in a group wins and enters the next turn. All the losers in this turn are ranked the same. Every N
-​G
-​​  winners are then grouped in the next match until a final winner is determined.
+#include <iostream>
+#include <queue>
+#include <algorithm>
 
-For the sake of simplicity, assume that the weight of each mouse is fixed once the programmer submits his/her code. Given the weights of all the mice and the initial playing order, you are supposed to output the ranks for the programmers.
+using namespace std;
 
-Input Specification:
-Each input file contains one test case. For each case, the first line contains 2 positive integers: N
-​P
-​​  and N
-​G
-​​  (≤1000), the number of programmers and the maximum number of mice in a group, respectively. If there are less than N
-​G
-​​  mice at the end of the player's list, then all the mice left will be put into the last group. The second line contains N
-​P
-​​  distinct non-negative numbers W
-​i
-​​  (i=0,⋯,N
-​P
-​​ −1) where each W
-​i
-​​ is the weight of the i-th mouse respectively. The third line gives the initial playing order which is a permutation of 0,⋯,N
-​P
-​​ −1 (assume that the programmers are numbered from 0 to N
-​P
-​​ −1). All the numbers in a line are separated by a space.
+struct Mouse {
+    int rank;
+    int weight;
+};
 
-Output Specification:
-For each test case, print the final ranks in a line. The i-th number is the rank of the i-th programmer, and all the numbers must be separated by a space, with no extra space at the end of the line.
+int main() {
+    int np, ng, order;
+    scanf("%d%d", &np, &ng);
+    Mouse mouse[np];
 
-Sample Input:
-11 3
-25 18 0 46 37 3 19 22 57 56 10
-6 0 8 7 10 5 9 1 4 2 3
-Sample Output:
-5 5 5 2 5 5 5 3 1 3 5
- * */
+    queue<int> q;
+
+    for (int i = 0; i < np; i++)
+        scanf("%d", &mouse[i].weight);
+    for (int i = 0; i < np; i++) {
+        scanf("%d", &order);
+        q.push(order);
+    }
+
+    //本轮总老鼠数和组数
+    int num = np, group;
+    //最后一只老鼠特殊处理
+    while (q.size() > 1) {
+        group = num % ng == 0 ? num / ng : num / ng + 1;
+        //找出每一组的晋级者
+        for(int g=0; g<group;g++){
+            int winner = q.front();
+            //本组总数不足ng，结束循环
+//            printf("%d", q.front());
+            for(int m=0; m<ng && g*ng+m<num; m++){
+                if(mouse[q.front()].weight > mouse[winner].weight)
+                    winner = q.front();
+                mouse[q.front()].rank = group+1;
+                q.pop();
+            }
+            q.push(winner);
+        }
+        //每组有一只老鼠晋级
+        num = group;
+    }
+    //最后一只老鼠排名1
+    mouse[q.front()].rank = 1;
+
+    for(int i=0; i<np; i++){
+        printf("%d", mouse[i].rank);
+        if(i<np-1)
+            printf(" ");
+    }
+
+    return 0;
+}
+
+//15:40 AC
