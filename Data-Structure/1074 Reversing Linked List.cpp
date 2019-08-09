94bffa980b160eb2b6f8cf964bb54543f6cf018a
egan chen <1154610080@qq.com>
2019-08-09 12:35:13 +0800

    完成数据结构专项练习:1074 Reversing Linked List

--- Data-Structure/1074 Reversing Linked List.cpp
+++ Data-Structure/1074 Reversing Linked List.cpp
@@ -26,6 +26,15 @@ Sample Input:
 33218 3 00000
 99999 5 68237
 12309 2 33218
+
+00100 6 2
+00000 4 99999
+00100 1 12309
+68237 6 -1
+33218 3 00000
+99999 5 68237
+12309 2 33218
+
 Sample Output:
 00000 4 33218
 33218 3 12309
@@ -33,5 +42,86 @@ Sample Output:
 00100 1 99999
 99999 5 68237
 68237 6 -1
- * */
 
+
+00000 6 3
+00000 1 11111
+11111 2 22222
+22222 3 -1
+33333 4 44444
+44444 5 55555
+55555 6 -1
+ * */
+//2019.8.8 17:00 reading
+//2019.8.8 17:06 read
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    int next;
+} node[100010];
+
+int n, k;
+
+int reverse(int head, int valid) {
+
+
+    int reverseBegin = 0;
+
+    //划分为n/k块，每k个反转
+    for (int i = 0,temp; i < (valid / k); i++) {
+        //本块开始地址，下一个地址，下下个地址
+        int blockBegin, next, afterNext;
+        next = node[head].next;
+        blockBegin = next;
+        afterNext = node[next].next;
+
+        //反转本块
+        for (int j = 1; j < k; j++) {
+            temp = node[afterNext].next;
+            node[afterNext].next = next;
+            next = afterNext;
+            afterNext = temp;
+        }
+        node[node[head].next].next = afterNext;
+        node[head].next = next;
+        head = blockBegin;
+
+        if (i == 0) {
+            reverseBegin = next;
+        }
+    }
+
+    return reverseBegin;
+}
+
+int main() {
+    int head, address, last, next;
+    scanf("%d%d%d", &head, &n, &k);
+    for (int i = 0; i < n; i++) {
+        scanf("%05d", &address);
+        scanf("%d%d", &node[address].data, &node[address].next);
+    }
+    //计算有效节点数
+    int h = head, valid = 0;
+    while (h != -1) {
+        h = node[h].next;
+        valid++;
+    }
+
+    //哨兵
+    node[100001].next = head;
+    head = reverse(100001, valid);
+
+    while (head != -1) {
+        printf("%05d %d ", head, node[head].data);
+        node[head].next != -1 ? printf("%05d", node[head].next) : printf("-1");
+        printf("\n");
+        head = node[head].next;
+    }
+}
+
+//+1 12:28 PAT AC
