918a9486cc968183caa1465e55724153db6aee5e
egan chen <1154610080@qq.com>
2019-08-12 17:25:00 +0800

    完成搜索专项练习:1103 Integer Factorization

---
+++ Search/1103 Integer Factorization.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Administrator on 2019/8/12.
+//
+
+//2019.8.12 16:33 reading
+//2019.8.12 16:38 read
+
+/**
+ *
+Sample Input 1:
+169 5 2
+Sample Output 1:
+169 = 6^2 + 6^2 + 6^2 + 6^2 + 5^2
+Sample Input 2:
+169 167 3
+Sample Output 2:
+Impossible
+ */
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int n, k, p, maxFac = -1;
+//fac方便高速计算
+vector<int> fac, temp, ans;
+
+void initFac() {
+    for (int i = 1, f = 0; f <= n; i++) {
+        fac.push_back(f);
+        f = (int) pow(i, p);
+    }
+
+}
+
+void dfs(int index, int nowK, int sum, int sumFac) {
+    if (nowK == k && sum == n) {
+        if (sumFac > maxFac) {
+            ans = temp;
+            maxFac = sumFac;
+        }
+        return;
+    }
+    if (nowK > k || sum > n) return;
+    if (index - 1 >= 0) {
+        temp.push_back(index);
+        dfs(index, nowK+1, sum + fac[index], sumFac + index);
+        temp.pop_back();
+        dfs(index - 1, nowK, sum, sumFac);
+    }
+}
+
+int main() {
+
+    scanf("%d%d%d", &n, &k, &p);
+    initFac();
+    dfs(fac.size()-1, 0, 0, 0);
+    if(maxFac == -1) printf("Impossible\n");
+    else{
+        printf("%d = %d^%d", n, ans[0], p);
+        for(int i=1; i<ans.size(); i++)
+            printf(" + %d^%d", ans[i], p);
+    }
+}
