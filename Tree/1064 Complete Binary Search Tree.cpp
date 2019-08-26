8faa0ee0c590289d26ac19bc3f4f5a0597cfa799
egan chen <1154610080@qq.com>
2019-08-26 15:10:50 +0800

    完成二叉查找树专项练习:1064 Complete Binary Search Tree

--- Tree/1064 Complete Binary Search Tree.cpp
+++ Tree/1064 Complete Binary Search Tree.cpp
@@ -2,3 +2,46 @@
 // Created by Administrator on 2019/8/26.
 //
 
+//2019.8.26 14:46
+
+/**
+Sample Input:
+10
+1 2 3 4 5 6 7 8 9 0
+Sample Output:
+6 3 8 1 5 7 9 0 2 4
+**/
+
+#include <queue>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int key[1010], CBT[1010], n, index=0;
+
+void create(int root){
+    if(root > n) return;
+    create(root*2);
+    CBT[root] = key[index++];
+    create(root*2+1);
+}
+
+
+int main()
+{
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &key[i]);
+    }
+
+    sort(key, key+n);
+
+    create(1);
+    printf("%d", CBT[1]);
+    for(int i=2; i<=n;i++){
+        printf(" %d", CBT[i]);
+    }
+}
+
+//15:10 AC
